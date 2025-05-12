1b0067c806963201dc4e43642c046773c9128495
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2025-05-12 10:17:38 +0200

    Modify chktemp.sh to use read instead of cat

--- chktemp.sh
+++ chktemp.sh
@@ -2,16 +2,13 @@
 
 res=0
 i=0
-
 for t in /sys/class/thermal/thermal_zone*/temp
-do 
-    let t=$(cat $t)
+do
+    read t < $t
     let t/=100
     let res+=t
     let i++
 done
-
 let res/=i
 echo Average temperature across $i cores:
 echo $res | sed 's/.$/.&°C/'
-

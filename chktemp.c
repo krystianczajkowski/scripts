58169da1b4572a4879e156f44c4a159ac98e85ea
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2025-05-12 10:19:19 +0200

    Add C program for easy compilation of chktemp to binary

--- chktemp.c
+++ chktemp.c
@@ -1,8 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("Temp:\n");
-    system("cat /sys/class/thermal/thermal_zone*/temp");
-    return 0;
+#define BASH_SCRIPT "\
+#!/bin/bash \n\
+res=0;i=0 \n\
+for t in /sys/class/thermal/thermal_zone*/temp \n\
+do \n\
+    read t < $t \n\
+    t=$((t/100)) \n\
+    res=$((res+t)) \n\
+    i=$((i+1)) \n\
+done \n\
+res=$((res/i)) \n\
+echo Average temperature across $i cores: \n\
+echo $res | sed 's/.$/.&°C/' \n\
+"
+
+int main(void) {
+    return system(BASH_SCRIPT);
 }
